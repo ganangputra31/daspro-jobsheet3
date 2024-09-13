25b7ca20646046bb25618a83be812a5773798ecc
ganang andika kurnia putra <myinwork14@gmail.com>
2024-09-13 13:26:51 +0700

    simpan kode Java ke Github

--- Siakad08.java
+++ Siakad08.java
@@ -4,30 +4,37 @@ public class Siakad08 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String nama,nim;
-        char kelas;
+        String kelas;
         byte absen;
-        double nilaiKuis, nilaiTugas, nilaiUjian, nilaiAkhir;
+        double nilaiKuis, nilaiTugas, nilaiUTS, nilaiUAS, nilaiAkhir;
 
         System.out.print("Masukkan Nama: ");
         nama = sc.nextLine();
         System.out.print("Masukkan Nim: ");
         nim = sc.nextLine();
         System.out.print("Masukkan Kelas: ");
-        kelas = sc.next().charAt(0);
+        kelas = sc.nextLine();
         System.out.print("Masukkan nomor Absen:");
         absen = sc.nextByte();
+        
+
 
         System.out.print("Masukkan Nilai kuis: ");
         nilaiKuis = sc.nextDouble();
         System.out.print("Masukkan Nilai Tugas: ");
         nilaiTugas = sc.nextDouble();
-        System.out.print("Masukkan Nilai Ujian: ");
-        nilaiUjian = sc.nextDouble();
+        System.out.print("Masukkan Nilai UTS: ");
+        nilaiUTS = sc.nextDouble();
+        System.out.print("Masukkan Nilai UAS: ");
+        nilaiUAS = sc.nextDouble();
+        System.out.print("Masukkan Nilai Akhir: ");
+        nilaiAkhir = sc.nextDouble();
 
-        nilaiAkhir = (nilaiKuis+nilaiTugas+nilaiUjian)/3;
 
-        System.out.println("Nama:" + nama + "Nim: " + nim);
-        System.out.println("Kelas: " + kelas + "Absen: " + absen);
+        nilaiAkhir = (nilaiKuis * 0.20) + (nilaiTugas * 0.15) + (nilaiUTS * 0.30) + (nilaiUAS * 0.35);
+
+        System.out.println("Nama:" + nama + " Nim: " + nim);
+        System.out.println("Kelas: " + kelas + " Absen: " + absen);
         System.out.println("Nilai Akhir: " + nilaiAkhir);
     
 
